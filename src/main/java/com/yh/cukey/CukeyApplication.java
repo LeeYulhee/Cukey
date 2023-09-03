ffddb1105541f13b1a3de6d6cd9676654040740e
LeeYulhee <reasonoflife39@gmail.com>
2023-09-04 02:15:20 +0900

    feat : 프로젝트 초기 세팅

---
+++ src/main/java/com/yh/cukey/CukeyApplication.java
@@ -0,0 +1,13 @@
+package com.yh.cukey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CukeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yh/cukey/CukeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yh.cukey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CukeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
